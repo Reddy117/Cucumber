d24a590fd34424d27e5b01f4f59a9ef754752649
pc <pc@DESKTOP-OCIILT7>
2018-01-19 09:05:27 +0530

    c

--- src/com/cucumber/runner/KartRunner.java
+++ src/com/cucumber/runner/KartRunner.java
@@ -5,6 +5,9 @@ import org.junit.runner.RunWith;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
+//@ExtendedCucumberOptions(jsonReport = "target/cucumber.json",
+//overviewReport = true,
+//outputFolder = "target")
 @Cucumber.Options(features="src\\com\\cucumber\\features\\Kart.feature",
 glue="com.cucumber.stepdefination",format = { "html:target/cucumber-html-report"}
 //,tags="~@Tag1"
